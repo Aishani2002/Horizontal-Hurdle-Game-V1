d7434f4833711aac090603c0b224510a8ef8f702
Aishani2002 <61320981+Aishani2002@users.noreply.github.com>
2020-04-23 17:57:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var gameState = 0;
+var playerCount,database,form,player,game;
+var allPlayers;
+
+
+function setup(){
+  createCanvas (displayWidth,displayHeight);
+
+  database = firebase.database();
+  game= new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+if (playerCount===4){
+  game.update(1);
+}
+
+if(gameState===1){
+  clear()
+  game.play();
+}
+}
